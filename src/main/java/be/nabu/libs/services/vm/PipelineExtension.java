b5c920865cb2d01a7e4586e56a55de5fbc261228
Alexander Verbruggen <alex@nabu.be>
2017-06-19 09:55:48 +0200

    - Updated pipeline extension so it updates the parent when elements are pushed upwards - Reverted name-only auditing as this breaks trace mode and possibly other tools

--- src/main/java/be/nabu/libs/services/vm/PipelineExtension.java
+++ src/main/java/be/nabu/libs/services/vm/PipelineExtension.java
@@ -2,8 +2,10 @@ package be.nabu.libs.services.vm;
 
 import java.util.List;
 
+import be.nabu.libs.types.api.ComplexType;
 import be.nabu.libs.types.api.Element;
 import be.nabu.libs.types.api.ModifiableComplexType;
+import be.nabu.libs.types.api.ModifiableElement;
 import be.nabu.libs.types.base.ValueImpl;
 import be.nabu.libs.types.properties.TemporaryProperty;
 import be.nabu.libs.types.structure.Structure;
@@ -20,6 +22,9 @@ public class PipelineExtension extends Structure {
 	
 	public List<ValidationMessage> add(Element<?> element, boolean setInSuper) {
 		if (setInSuper && getSuperType() != null) {
+			if (element instanceof ModifiableElement) {
+				((ModifiableElement<?>) element).setParent((ComplexType) getSuperType());
+			}
 			if (getSuperType() instanceof PipelineExtension) {
 				return ((PipelineExtension) getSuperType()).add(element, setInSuper);
 			}
